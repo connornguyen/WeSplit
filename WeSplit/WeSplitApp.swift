0d9c5e84fd60aac37e8165616d1e5a1b79460673
connornguyen <nguyendinhthanhcong2512@gmail.com>
2024-07-12 13:54:36 -0700

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Apple on 12/7/24.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
